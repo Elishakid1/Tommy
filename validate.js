65b76a27addc3eaf2c3282d5661c4e4dd521fb7b
Elishakid1 <141440573+Elishakid1@users.noreply.github.com>
2025-01-01 17:27:10 -0800

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,9 @@
+function check(form) {
+  if (form.userid.value == "christina0x1" && form.pwd.value == "christina0x1") {
+    return true;
+  } else {
+    alert("Password e nome utente errati")
+    return false;
+  }
+}
+
